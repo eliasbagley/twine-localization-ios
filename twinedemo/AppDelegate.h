68d69b0fdf97c7b8ef0b30189df78632cca66f30
Elias Bagley <elias@rocketmade.com>
2013-10-03 14:22:18 -0600

    Initial Commit

---
+++ twinedemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  twinedemo
+//
+//  Created by Elias Bagley on 10/3/13.
+//  Copyright (c) 2013 Elias Bagley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ twinedemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  twinedemo
+//
+//  Created by Elias Bagley on 10/3/13.
+//  Copyright (c) 2013 Elias Bagley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
